be40b9241f027d252be5e27c0068c95ccf48ed00
softeyes-xl <767733103@qq.com>
2018-03-03 20:34:04 +0800

    2018.3.3

--- js/control.js
+++ js/control.js
@@ -1,4 +1,8 @@
 $('#myBtn').on('click',function() {
+    getMyId();
+})
+
+function getMyId() {
     haveId = false;
     getId = null;
     clearTimeout(loadTimer);
@@ -40,8 +44,40 @@ $('#myBtn').on('click',function() {
         }
 
     }
-})
+}
+
+document.onkeydown=function(event){
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    if(e && e.keyCode==27){ // 按 Esc 
+        //要做的事情
+      }
+    if(e && e.keyCode==113){ // 按 F2 
+         //要做的事情
+       }            
+     if(e && e.keyCode==13){ // enter 键
+         //要做的事情
+         getMyId();
+    }
+}; 
+
+function throttle(method,delay){
+    var timer=null;
+    return function(){
+        var context=this, args=arguments;
+        clearTimeout(timer);
+        timer=setTimeout(function(){
+            method.apply(context,args);
+        },delay);
+    }
+}
 
 $(window).resize(function() {
-    ajaxOk();
+    for(var i=0; i<$('.mydiv').length; i++) {
+        $('.mydiv')[i].style = '';
+    }
+    clearTimeout(reTimer);
+    reTimer = setTimeout(function() {
+        ajaxOk();     
+    },500)
+
 })
